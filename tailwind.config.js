705c3358ac41295aec69736a971a0f40331fe7f5
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-12-06 09:50:33 -0600

    build: design system
    Includes typography, colors, and image assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      red: '#F15D51',
+      yellow: '#E9AA52',
+      black: '#00001A',
+      silver: '#C5C6CE',
+      gray: '#5E607A',
+      white: '#FFFDFA',
+    },
+    fontSize: {
+      body: ['15px', '26px'],
+      xs: ['18px', '24px'],
+      sm: ['20px', '24px'],
+      md: ['32px', '32px'],
+      lg: ['40px', '40px'],
+      xl: ['56px', '56px'],
+    },
     extend: {},
   },
   plugins: [],
